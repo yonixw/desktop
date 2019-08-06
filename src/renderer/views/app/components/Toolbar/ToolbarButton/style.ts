0d201a040581473c28fe1da6ad20e02421e6d552
Sential <sentialx@gmail.com>
2019-08-07 01:14:51 +0200

    style: remove background image transition

--- src/renderer/views/app/components/Toolbar/ToolbarButton/style.ts
+++ src/renderer/views/app/components/Toolbar/ToolbarButton/style.ts
@@ -7,8 +7,6 @@ import { ITheme } from '~/interfaces';
 export const Icon = styled.div`
   width: 100%;
   height: 100%;
-  will-change: background-image;
-  transition: 0.15s background-image;
   backface-visibility: hidden;
 
   ${({
